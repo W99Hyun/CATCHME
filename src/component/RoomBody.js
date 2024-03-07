563ec25460e0b619c5ae77785298750b0a29633e
ki <redcedar1@naver.com>
2024-03-07 19:51:01 +0900

    secure websocket test

--- src/component/RoomBody.js
+++ src/component/RoomBody.js
@@ -161,7 +161,6 @@ const [totalCondition, setTotalCondition] = useState(null);
     setIsReady(!isReady);
   };
 
-  /*
   useEffect(() => {
     if (user && isReady && !dataSocket.current) {
       // 레디 상태일 때 웹소켓 연결
@@ -222,7 +221,6 @@ const [totalCondition, setTotalCondition] = useState(null);
       }
     };
   }, []);
-  */
 
   const [showReadyConfirmModal, setShowReadyConfirmModal] = useState(false);
   const [showModal, setShowModal] = useState(false);
